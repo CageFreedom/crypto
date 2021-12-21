f4ba0a01bff57dbc2fa840969f875a795ee2d9a2
cagefreedom <shenwj@115bit.com>
2021-12-21 18:13:22 +0800

    add crypto-trades.py

---
+++ crypto-trades.py
@@ -0,0 +1,36 @@
+from typing import Collection
+from cryptofeed import FeedHandler
+from cryptofeed.backends.mongo import CandlesMongo, TradeMongo
+from cryptofeed.defines import TRADES, CANDLES
+from cryptofeed.exchanges import Binance, Coinbase
+
+def main():
+    f = FeedHandler()
+    f.add_feed(
+        Binance(
+            retries = -1,
+            # candle_interval = '1h',
+            channels=[TRADES],
+            symbols=['BTC-USDT','ETH-USDT','SOL-USDT',"MATIC-USDT"],
+            callbacks={
+                TRADES: TradeMongo('binance', collections='trades'),
+                # CANDLES: CandlesMongo('binance', collection='candles')
+            }
+        )
+    )
+
+    f.add_feed(
+        Coinbase(
+            retries = -1,
+            channels=[TRADES],
+            symbols=['BTC-USD','ETH-USD','SOL-USD','MATIC-USD'],
+            callbacks={
+                TRADES: TradeMongo('coinbase', collections='trades'),
+            }
+        )
+    )
+
+    f.run()
+
+if __name__ == '__main__':
+    main()
